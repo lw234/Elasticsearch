e0f8a1acda027022478f834b89cd51f4fa69045a
lw234 <lynn291991@yahoo.com>
2014-12-11 12:49:43 -0500

    create a twitter search box

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE php>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> Elastic Search</title>
+
+	</head>
+	<body>
+		<form action="index.php" method= "get" autocomplete="off">
+			<Label>
+				Search Tweets Timeline 
+				<input type="text" name="q">
+			</Label>
+			<input type="submit" value="Search">
+		</form>
+	</body>
+</html>
